1a61014eaad1c4ebf04bc4c8c0a6fe3827ce4214
DesPlusVision <suporte@plusvision.com.br>
2014-12-23 22:27:28 -0300

    Adicionando uma nova ROTA que passa os dados do metodo da Classe EmpresaController para a tela de Login.

--- app/routes.php
+++ app/routes.php
@@ -1,19 +1,10 @@
 <?php
 
-/*
-  |--------------------------------------------------------------------------
-  | Application Routes
-  |--------------------------------------------------------------------------
-  |
-  | Here is where you can register all of the routes for an application.
-  | It's a breeze. Simply tell Laravel the URIs it should respond to
-  | and give it the Closure to execute when that URI is requested.
-  |
- */
-
 Route::get('/', function() {
     return View::make('login');
 });
+
+Route::get('/', 'EmpresaController@getEmpresasLogin');
 
 Route::post('/', 'UsuarioController@verificarUsuario');
 
